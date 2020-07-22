7700b4845ae28a19d63486247a7139ff4ee7234d
Micho <mauricio.castro@mail.udp.cl>
2020-07-21 23:33:49 -0400

    Initial commit (not functional yet)

---
+++ generic-data-model/generic-data-model/DataInterpreter/DataInterpreter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModelParty.DataInterpreter
+{
+    public abstract class DataInterpreter
+    {
+        public abstract T GetField<T>(string fieldName);
+
+        public abstract IList<T> GetArrayField<T>(string fieldName);
+
+        public abstract bool SetArrayField<T>(string fieldName, IList<T> list);
+
+        public abstract bool SetField<T>(string fieldName, T field);
+
+
+    }
+}
